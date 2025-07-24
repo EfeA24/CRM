22c4fb9cb0841d2bb4b99c4271a270470cb151b8
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2025-07-25 02:42:40 +0300

    Quary Update

---
+++ Crm.CRUD/CRM.Core/Crm.Core.Application/Features/CQRS/Queries/PurchaseQueries/GetPurchaseOfferDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using Crm.Core.Application.Features.CQRS.Results.OfferResults.PurchaseOffer;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Core.Application.Features.CQRS.Queries.PurchaseQueries
+{
+    public class GetPurchaseOfferDetailQuery : IRequest<PurchaseOfferDetailResult>
+    {
+    }
+}
---
+++ Crm.CRUD/CRM.Core/Crm.Core.Application/Features/CQRS/Queries/SaleQueries/GetAllSaleOffersQuery.cs
@@ -0,0 +1,14 @@
+﻿using Crm.Core.Application.Features.CQRS.Results.OfferResults.SaleOffer;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Core.Application.Features.CQRS.Queries.SaleQueries
+{
+    public class GetAllSaleOffersQuery : IRequest<List<SalesOfferListResult>>
+    {
+    }
+}
