08d0e3c15f9fbd7599c34c2f87f5852946fcc7c6
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2025-08-05 22:28:37 +0300

    update

---
+++ Crm.Redis/Crm.Redis.Application/Interfaces/IWebhookClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Redis.Application.Interfaces
+{
+    internal class IWebhookClient
+    {
+    }
+}
